8d57e759c1bd0e54bf1645af326d64f31e07cf2a
johan sinaga <johanfransiskussinaga@gmail.com>
2025-02-13 14:34:48 +0700

    user register

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\View\View;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     */
+    public function create(): View
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255', 'unique:users,username'],
+            'whatsapp' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $phone = $request->whatsapp;
+
+        $phone = preg_replace('/\D/', '', $phone);
+
+        // Jika nomor diawali "0", ganti dengan "62"
+        if (substr($phone, 0, 1) === '0') {
+            $phone = '62' . substr($phone, 1);
+        }
+
+        // Jika nomor diawali "+62", hapus "+"
+        if (substr($phone, 0, 3) === '620') {
+            $phone = '62' . substr($phone, 2);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'whatsapp' => $phone,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return redirect(route('dashboard', absolute: false));
+    }
+}
